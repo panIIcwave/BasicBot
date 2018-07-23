e17a66b54faf1e3809241efa80704ca551756783
panIIcwave <41567484+panIIcwave@users.noreply.github.com>
2018-07-23 04:45:56 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,37 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, { colorize: true });
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.client({
+	token: auth.token,
+	autorun: true
+});
+bot.on('ready', function (evt) {
+	logger.info('Connected');
+	logger.info('Logged in as: ');
+	logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+	// Our bot needs to know if it will execute a command
+	// It will listen to messages that will start with 'Alexa'
+	if (message.substring(0, 1) == 'Alexa') {
+		var args = message.substring(1).split(' ');
+		var cmd = args[0];
+
+		args = args.splice(1);
+		switch(cmd) {
+			// saying Alexa will trigger it
+			case 'Alexa'
+				bot.sendMessage({
+					to: chanelID,
+					message: 'playing Despacito.'
+				});
+			break;
+			// Just add any case commands if you want to...
+		}
+	}
+});
